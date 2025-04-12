ec6d04eaebc750e311c07c7ce05803fc93222125
Ibnub00 <ibnugifarila@gmail.com>
2025-04-13 02:46:17 +0700

    first commit

---
+++ todolist/src/app/lib/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// isi konfigurasi sesuai dengan konfigurasi firebase kalian
+const firebaseConfig = {
+    apiKey: "AIzaSyAGXg84VXV5aL3JN459iyxzJj5IQbOmdG0",
+    authDomain: "todolist-b02d1.firebaseapp.com",
+    projectId: "todolist-b02d1",
+    storageBucket: "todolist-b02d1.firebasestorage.app",
+    messagingSenderId: "473066538552",
+    appId: "1:473066538552:web:9650fb64684689d3d208cd",
+    measurementId: "G-3RMERTWMK0"
+};
+
+// Inisialisasi Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
+
